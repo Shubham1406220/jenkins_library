fcc7069160e876a793860d03c2c12acaef897f8d
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 00:30:18 +0530

    Create tomcat_deploy.groovy

---
+++ vars/tomcat_deploy.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+def tomcat () {
+  pom = readMavenPom file: 'pom.xml'
+  deploy adapters: 
+    [tomcat8(credentialsId: '87de1842-3bb6-46d6-b0ba-003da5333f5b', 
+    path: '', url: 'http://13.233.62.12:8080')], 
+    contextPath: null, 
+    onFailure: false, 
+    war: '${pom.artifcatID}.${pom.packaging}'
+}
+
+
