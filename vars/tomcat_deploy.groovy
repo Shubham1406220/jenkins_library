aa8b64827251ec88a288255338524563cf68560f
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 01:00:09 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringUtils
 
 def call(){
   pom = readMavenPom file: 'pom.xml'
+  echo "FILE: ${pom.artifactID} , PACKAGE: ${pom.packaging}, VERSION : ${pom.version}"
   deploy adapters: 
     [tomcat8(credentialsId: '87de1842-3bb6-46d6-b0ba-003da5333f5b', 
     path: '', url: 'http://13.233.62.12:8080')], 
