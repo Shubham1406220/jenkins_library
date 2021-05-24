6e1d09d9179168987f5e72757466f799d574df55
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 00:43:37 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def tomcat () {
+def tomcat(){
   pom = readMavenPom file: 'pom.xml'
   deploy adapters: 
     [tomcat8(credentialsId: '87de1842-3bb6-46d6-b0ba-003da5333f5b', 
