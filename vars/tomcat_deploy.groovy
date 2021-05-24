5904a532e4a89a578a080f9da3fa5e15cd7f3232
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 01:08:38 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -10,7 +10,7 @@ def call(){
     path: '', url: 'http://13.233.62.12:8080')], 
     contextPath: '.', 
     onFailure: false, 
-    war: """${pom.artifactId}.${pom.packaging}"""
+    war: "${pom.artifactId}-${pom.version}.${pom.packaging}"
 }
 
 
