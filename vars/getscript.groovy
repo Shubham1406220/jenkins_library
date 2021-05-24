eed534d9f651420e9a80d353c8f703b6c85d9a2e
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-24 23:16:10 +0530

    Update getscript.groovy

--- vars/getscript.groovy
+++ vars/getscript.groovy
@@ -1,5 +1,5 @@
 def call (Map name = [:]) {
-  def content = libraryResources "/helloworld.sh"
+  def content = libraryResource "/helloworld.sh"
   writeFile file: "helloworld.sh", text: content
   sh "chmod +x ./helloworld.sh"
   sh "./helloworld.sh ${name.name} ${name.year}"
