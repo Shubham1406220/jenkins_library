933dda95a67854820c8d7f0b4e6bfda524cdaf84
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 01:10:36 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -5,12 +5,13 @@ import org.apache.commons.lang.StringUtils
 def call(){
   pom = readMavenPom file: 'pom.xml'
   echo "FILE: ${pom.artifactId} , PACKAGE: ${pom.packaging}, VERSION : ${pom.version}"
+  echo "Warfile: ${pom.artifactId}-${pom.version}.${pom.packaging}"
   deploy adapters: 
     [tomcat8(credentialsId: '87de1842-3bb6-46d6-b0ba-003da5333f5b', 
     path: '', url: 'http://13.233.62.12:8080')], 
     contextPath: '.', 
     onFailure: false, 
-    war: "${pom.artifactId}-${pom.version}.${pom.packaging}"
+    war: "/targt/${pom.artifactId}-${pom.version}.${pom.packaging}"
 }
 
 
