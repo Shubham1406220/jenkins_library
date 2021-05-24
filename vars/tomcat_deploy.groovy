182e57a520827a99d9f7dfb92e3523244308a6c9
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 01:13:31 +0530

    Update tomcat_deploy.groovy

--- vars/tomcat_deploy.groovy
+++ vars/tomcat_deploy.groovy
@@ -9,7 +9,7 @@ def call(){
   deploy adapters: 
     [tomcat8(credentialsId: '87de1842-3bb6-46d6-b0ba-003da5333f5b', 
     path: '', url: 'http://13.233.62.12:8080')], 
-    contextPath: '.', 
+    contextPath: null, 
     onFailure: false, 
     war: "${pom.artifactId}-${pom.version}.${pom.packaging}"
 }
