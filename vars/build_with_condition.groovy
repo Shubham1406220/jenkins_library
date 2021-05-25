69c5ca6afcc04fb31aa9fdb2769380ac14e589e3
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 20:55:54 +0530

    Update build_with_condition.groovy

--- vars/build_with_condition.groovy
+++ vars/build_with_condition.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(){
- if (fileExists ('${params.Directory}/pom.xml'))
+ if (fileExists('${params.Directory}/pom.xml'))
   {
     cd "**/${params.Directory}"
     build("package")
