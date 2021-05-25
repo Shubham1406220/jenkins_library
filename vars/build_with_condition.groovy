6b3a0d18633c584a3446d301b7adbb694a89210b
Shubham1406220 <76969039+Shubham1406220@users.noreply.github.com>
2021-05-25 18:56:15 +0530

    Create build_with_condition.groovy

---
+++ vars/build_with_condition.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def call() {
+ def file = findFiles excludes: '', glob: 'pom.xml'
+ if ( fileExists '/${params.Directory}/pom.xml' )
+  {
+    build("package")
+  }
+  else {
+    echo "POM.XML not present"
+  }
+}
